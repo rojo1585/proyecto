a2f77cae55631e2b25a24337aea237ee4e8b2f67
20177717 MENDOZA CORREA JORGE MIGUEL <20177717@TELEMATICA.UCOL.MX>
2020-03-12 08:42:23 -0600

    a

--- 10-3/nonbres/app.js
+++ 10-3/nonbres/app.js
@@ -1,11 +1,11 @@
-var  randomNum = require('random-numbers-generators');
-var direccions = require('./address.js')
+const  randomNum = require('random-numbers-generators');
+const direccions = require('./address.js')
 
 const request = require('request');
-
+//conexion con l aapi para conseguir un nombre random 
 request('https://swapi.co/api/people/'+randomNum(5)+'/', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log('body:',body)
+  if (err) { return console.log(err); }//manejo de error
+  console.log('body:',body)//los datos obtenido se almacenan en boby
 });
 
 direccions(1);
--- 10-3/nonbres/geo.js
+++ 10-3/nonbres/geo.js
@@ -1,9 +1,8 @@
 const request = require('request');
 const address = require('./address.js')
-
 b = address(1);
 console.log(b.StreetAddress);
-
+//conexion con la api para confirmar si el dato obtenido del modulo "adrres" existe
 request(`https://api.mapbox.com/geocoding/v5/mapbox.places/{b.StreetAddress}?access_token=pk.eyJ1Ijoicm9qbzE1ODUiLCJhIjoiY2s3bTAxdGljMGR1YzNkbzJqcnVta2Z5NSJ9.n59DT2Y3acqCEOlqplNt3A`, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log('body:',body)
